c3601f8ba686554502e2dfe5b242dfd67a2e29c3
jaesik-kim <jaesik.kim@shinstarr.com>
2024-08-22 13:59:41 +0900

    feat: java-start set

---
+++ src/CommentJava.java
@@ -0,0 +1,8 @@
+public class CommentJava {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, Java1"); // hello java 1 comment
+        System.out.println("Hello, Java2"); // hello java 2 comment
+        System.out.println("Hello, Java3"); // hello java 3 comment
+    }
+}
